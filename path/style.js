3940cbc69607cc463522d07d37328e8b32a657fd
shinjihye <twg0226@gmail.com>
2023-09-25 21:43:17 +0900

    style component 경로 묶음

---
+++ path/style.js
@@ -0,0 +1,9 @@
+import commonStyle from '../public/ui/common';
+import buttonStyle from '../public/ui/button';
+import colorStyle from '../public/ui/color';
+
+export default {
+  commonStyle,
+  buttonStyle,
+  colorStyle,
+};
