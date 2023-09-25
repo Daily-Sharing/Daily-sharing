105699df5f4f5f6a0c89100f842ce7523e27d6be
shinjihye <twg0226@gmail.com>
2023-09-25 21:42:58 +0900

    head 파일로 묶는중

---
+++ path/head.js
@@ -0,0 +1,12 @@
+export * from 'expo-status-bar';
+export * from 'react-native';
+export * from 'react-native-safe-area-context';
+export * from '@react-navigation/native';
+export * from '@react-navigation/native-stack';
+export * from '@react-navigation/bottom-tabs';
+export * from '@expo/vector-icons';
+
+export * from 'react-native-safe-area-context';
+export * from 'expo-constants';
+export * from '@expo/vector-icons';
+export * from 'react-native-elements';
